0c8a0804ec3b89c004cdbef264841c84c9bd0067
Arian Khavari <ariankhavari@Arians-MacBook-Pro-2.local>
2025-02-14 09:53:09 -0500

    Updated Flask app to use PORT environment variable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from post import Post
-
+import os
 
 blogs = Post()
 
@@ -10,9 +10,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", posts=blogs.get_blogs())
 
+port = int(os.environ.get("PORT", 10000))
+
 @app.route('/post/<int:blog_id>')
 def blog_page(blog_id):
     return render_template("post.html", blog=blogs.specific_blog(blog_id))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
